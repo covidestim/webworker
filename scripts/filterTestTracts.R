17218aec74e6c0bcaee9e9f1492111522d567d69
Marcus Russi <marcusrussi@gmail.com>
2021-08-12 16:31:40 -0400

    Added metadata support to `filterTestTracts.R`

--- scripts/filterTestTracts.R
+++ scripts/filterTestTracts.R
@@ -8,18 +8,20 @@ suppressPackageStartupMessages( library(tidyverse) )
 glue('covidestim test-track filterer
 
 Usage:
-  {name} -o <output_path> --tracts <tracts_path> --key <key> <input_path>
+  {name} -o <output_path> --tracts <tracts_path> --key <key> --metadata <path> --writeMetadata <path> <input_path>
   {name} (-h | --help)
   {name} --version
 
 Options:
-  -o <output_path>       Where to save the filtered input data .csv
-  --tracts <tracts_path> Path to a .csv with column "tract". FIPS codes or state names
-  --key <key>            Must be "state" or "fips"
-  <input_path>           Path to a .csv with columns date, cases, deaths, fracpos
-  <summary_path>         The path to the summary .csv produced by a set of runs
-  -h --help              Show this screen.
-  --version              Show version.
+  -o <output_path>        Where to save the filtered input data .csv
+  --tracts <tracts_path>  Path to a .csv with column "tract". FIPS codes or state names
+  --key <key>             Must be "state" or "fips"
+  --metadata <path>       Where to find metadata generated during the cleaning process
+  --writeMetadata <path>  Where to write metadata generated during the filtering process
+  <input_path>            Path to a .csv with columns date, cases, deaths, fracpos
+  <summary_path>          The path to the summary .csv produced by a set of runs
+  -h --help               Show this screen.
+  --version               Show version.
 ', name = "filterTestTracts.R") -> doc
 
 args <- docopt(doc, version = '0.1')
@@ -53,6 +55,11 @@ input_data <- read_csv(args$input_path, col_types = input_data_colspec)
 
 pd()
 cli_alert_info('{unique(pull(input_data,args$key))%>%length} tracts in input data')
+
+ps("Reading metadata from {.file {args$metadata}}")
+metadata <- jsonlite::read_json(args$metadata, simplifyVector = T)
+pd()
+
 ps('Filtering input data')
 
 filtered_input_data <- filter(
@@ -66,4 +73,8 @@ ps('Writing filtered data to {.file {args$o}}')
 
 write_csv(filtered_input_data, args$o)
 
+pd()
+
+ps("Writing metadata to {.file {args$writeMetadata}}")
+jsonlite::write_json(metadata, args$writeMetadata, null = "null")
 pd()
