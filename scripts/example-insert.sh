6a8d6682032111431a694d8a95d0e76d10abd965
Marcus Russi <marcusrussi@gmail.com>
2022-06-21 12:20:21 -0400

    Minor new features for `insert.R`
    - `COVIDESTIM_ENDPOINT` passed as an environment variable
    - `[--method]`: now optional, default is `optimizing`
    - Cleaner API response parsing

--- scripts/example-insert.sh
+++ scripts/example-insert.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 export COVIDESTIM_JWT=$(cat ../../db/new-schema/AUTH_TOKEN)
+export COVIDESTIM_ENDPOINT=http://localhost:3000/rpc/insert_run
 
 ./insert.R \
   --input input.csv \
   --summary summary.csv \
   --key state \
-  --method method.csv \
   --run-date $1 \
-  --endpoint http://localhost:3000/rpc/insert_run \
   --save-mapping mapping.csv \
   --metadata metadata.json
--- scripts/insert.R
+++ scripts/insert.R
@@ -1,10 +1,8 @@
 #!/usr/bin/Rscript
 'Covidestim DB inserter
 
-Hmm does this work?
-
-Usage:
-  insert.R --input <path> --summary <path> --method <path> --metadata <path> --key <string> --run-date <date> [--endpoint <url>] [--save-mapping <path>]
+usage:
+  insert.R --input <path> --summary <path> [--method <path>] --metadata <path> --key <string> --run-date <date> [--save-mapping <path>]
   insert.R (-h | --help)
   insert.R --version
 
@@ -15,7 +13,6 @@ Options:
   --metadata <path>  Path to a JSON of metadata generated earlier in the pipeline
   --key <string>    Must be "state" or "fips" - the grouping key for the data
   --run-date <string>  YYYY-MM-DD
-  --endpoint <url>  URL of a Covidestim API [default: https://api2.covidestim.org/rpc/insert_run]
   --save-mapping <path>  Where to save a CSV mapping [geo_name, run_id]
   -h --help  Show this screen.
   --version  Show version.
@@ -67,6 +64,8 @@ method_df_colspec <- rlang::list2(
   method      = col_character()
 ) %>% do.call(cols, .)
 
+api_response_colspec <- cols(run_id = col_number())
+
 # The database doesn't use `fips` or `state` - instead, it uses a column
 # `geo_name` for both types of geographies. This functions renames
 # the `state`/`fips` column to `geo_name`.
@@ -76,15 +75,18 @@ cli_alert_info("Loading {.code input_df} from {.file {args$input}}")
 input_df <- read_csv(args$input, col_types = input_df_colspec)
 cli_alert_info("Loading {.code summary_sf} from {.file {args$summary}}")
 summary_df <- read_csv(args$summary, col_types = summary_df_colspec)
-cli_alert_info("Loading {.code method_df} from {.file {args$method}}")
-method_df <- read_csv(args$method, col_types = method_df_colspec)
 cli_alert_info("Loading {.code metadata_df} from {.file {args$metadata}}")
 metadata_df <- jsonlite::read_json(args$metadata, simplifyVector = T)
 
 input_df    <- rename_to_geo_name(input_df)
 summary_df  <- rename_to_geo_name(summary_df)
-method_df   <- rename_to_geo_name(method_df)
 metadata_df <- rename_to_geo_name(metadata_df)
+
+if (!is.null(args$method)) {
+  cli_alert_info("Loading {.code method_df} from {.file {args$method}}")
+  method_df <- read_csv(args$method, col_types = method_df_colspec)
+  method_df <- rename_to_geo_name(method_df)
+}
 
 # Get a nested tibble of all the data for each geography. The result of this
 # is a tibble with columns `geo_name` and `input`, where `input` is itself
@@ -97,12 +99,19 @@ metadata_df <- nest(metadata_df, metadata = !geo_name)
 # "state" or "fips".
 geo_type_from_key <- switch(args$key, "state" = "state", "fips" = "county")
 
+attach_method <- function(df) {
+  if (is.null(args$method))
+    return(mutate(df, method = "optimizing"))
+
+  left_join(df, method_df, by = "geo_name")
+}
+
 # Produce a tibble where each row contains all the fields needed for the
 # RPC call
 one_row_per_run <- summary_df %>%
   left_join(input_df,    by = "geo_name") %>%
-  left_join(method_df,   by = "geo_name") %>%
   left_join(metadata_df, by = "geo_name") %>%
+  attach_method(.) %>%
   mutate(
     run_date = args$run_date,
     geo_type = geo_type_from_key,
@@ -118,6 +127,16 @@ one_row_per_run <- summary_df %>%
     )
   )
     
+# API endpoint must be passed as an environment variable.
+get_endpoint <- function() {
+  endpoint <- Sys.getenv('COVIDESTIM_ENDPOINT')
+  if (identical(endpoint, "")) {
+    stop("Please set env var COVIDESTIM_ENDPOINT to your Covidestim API endpoint.")
+  }
+
+  endpoint
+}
+
 # JWT token that gives privileges for executing the `insert_run` function must
 # be passed as an environment variable.
 get_token <- function() {
@@ -130,7 +149,7 @@ get_token <- function() {
 }
 
 response <- POST(
-  args$endpoint,
+  get_endpoint(),
   add_headers(
     Authorization = paste0("Bearer ", get_token()),
 
@@ -147,22 +166,20 @@ response <- POST(
 if (status_code(response) != 200) {
   stop(
     sprintf(
-      "Covidestim API request failed [%s]\n%s\n<%s>", 
-      status_code(resp),
-      parsed$message,
-      parsed$documentation_url
+      "Covidestim API request failed [%s]\n%s\n", 
+      status_code(response),
+      content(response)
     ),
     call. = FALSE
   )
 }
 
-cli_alert_success("Call to {.url {args$endpoint}} succeeded")
-print(response)
+cli_alert_success("Call to {.url {get_endpoint()}} succeeded")
 
 if (!is.null(args$save_mapping)) {
   # save the mapping from geo_name to run_id
   cli_alert_info("Saving mapping to {.file {args$save_mapping}}")
   select(one_row_per_run, geo_name) %>%
-    bind_cols(content(response)) %>%
+    bind_cols(content(response, col_types = api_response_colspec)) %>%
     write_csv(args$save_mapping)
 }
